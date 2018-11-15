3eae710c9e5e1d88176e51f2d6a2c5f9ba772747
Unicorn Killa <ashogenov2014@yandex.ru>
2018-11-15 17:34:57 +0300

    first commit

---
+++ Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public Transform player;
+    Vector3 position;
+	
+	void Start () {
+        transform.position = player.position;
+	}
+	
+	
+	void Update () {
+        position = player.position;
+        position.z = -10f;
+        position.x = 0f;
+        transform.position = Vector3.Lerp(transform.position, position, 1f * Time.deltaTime);
+	}
+}
---
+++ Scripts/CubeMoving.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMoving : MonoBehaviour {
+
+    float horizontal;
+    Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            horizontal = Input.acceleration.x;
+        }
+        else
+        {
+            horizontal = Input.GetAxis("Horizontal");
+        }
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * 10f, rb.velocity.y);
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(transform.up * 15f, ForceMode2D.Impulse);
+        }
+    }
+}
